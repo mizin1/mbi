b2ed27402841710024841ff661824216fed252ba
Konrad Mizinski <konrad_1916@wp.pl>
2014-05-01 16:14:27 +0200

    pierwszy formularz

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Executing: cp mbi.xml /home/mizin/apache-tomcat-7.0.42/conf/Catalina/localhost/mbi.xml"
+
+cp mbi.xml /home/mizin/apache-tomcat-7.0.42/conf/Catalina/localhost/mbi.xml
+
+echo "Done."
---
+++ src/main/java/pl/waw/mizinski/mbi/InputForm.java
@@ -0,0 +1,133 @@
+package pl.waw.mizinski.mbi;
+
+public class InputForm {
+	
+	private String sequenceA;
+	private String sequenceB;
+	
+	private int breakBegin;
+	private int breakContinue;
+	
+	private int AA;
+	private int AG;
+	private int AC;
+	private int AT;
+	private int GG;
+	private int GC;
+	private int GT;
+	private int CC;
+	private int CT;
+	private int TT;
+
+	public String getSequenceA() {
+		return sequenceA;
+	}
+
+	public void setSequenceA(String sequenceA) {
+		this.sequenceA = sequenceA;
+	}
+
+	public String getSequenceB() {
+		return sequenceB;
+	}
+
+	public void setSequenceB(String sequenceB) {
+		this.sequenceB = sequenceB;
+	}
+
+	public int getBreakBegin() {
+		return breakBegin;
+	}
+
+	public void setBreakBegin(int breakBegin) {
+		this.breakBegin = breakBegin;
+	}
+
+	public int getBreakContinue() {
+		return breakContinue;
+	}
+
+	public void setBreakContinue(int breakContinue) {
+		this.breakContinue = breakContinue;
+	}
+
+	public int getAA() {
+		return AA;
+	}
+
+	public void setAA(int aA) {
+		AA = aA;
+	}
+
+	public int getAG() {
+		return AG;
+	}
+
+	public void setAG(int aG) {
+		AG = aG;
+	}
+
+	public int getAC() {
+		return AC;
+	}
+
+	public void setAC(int aC) {
+		AC = aC;
+	}
+
+	public int getAT() {
+		return AT;
+	}
+
+	public void setAT(int aT) {
+		AT = aT;
+	}
+
+	public int getGG() {
+		return GG;
+	}
+
+	public void setGG(int gG) {
+		GG = gG;
+	}
+
+	public int getGC() {
+		return GC;
+	}
+
+	public void setGC(int gC) {
+		GC = gC;
+	}
+
+	public int getGT() {
+		return GT;
+	}
+
+	public void setGT(int gT) {
+		GT = gT;
+	}
+
+	public int getCC() {
+		return CC;
+	}
+
+	public void setCC(int cC) {
+		CC = cC;
+	}
+
+	public int getCT() {
+		return CT;
+	}
+
+	public void setCT(int cT) {
+		CT = cT;
+	}
+
+	public int getTT() {
+		return TT;
+	}
+
+	public void setTT(int tT) {
+		TT = tT;
+	}
+}
